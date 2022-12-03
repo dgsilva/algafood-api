305733b4c7d7ffc9104f97b7580abe879a407f4d
dgsilva <diegorj93@gmail.com>
2022-12-02 22:37:34 -0300

    Usando @Configuration e @Bean para definir beans

--- src/main/java/com/algaworks/algafood/controllers/MeuPrimeiroController.java
+++ src/main/java/com/algaworks/algafood/controllers/MeuPrimeiroController.java
@@ -3,12 +3,28 @@ package com.algaworks.algafood.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.algaworks.algafood.modelo.Cliente;
+import com.algaworks.algafood.services.AtivacaoClienteService;
+
 @Controller
 public class MeuPrimeiroController {
+
+	private AtivacaoClienteService ativacaoClienteService;
+
+	public MeuPrimeiroController(AtivacaoClienteService ativacaoClienteService) {
+		this.ativacaoClienteService = ativacaoClienteService;
+
+		System.out.println("MeuPrimeiroController: " + ativacaoClienteService);
+	}
 
 	@GetMapping("/hello")
 	@ResponseBody
 	public String hello() {
-		return "Hello2!";
+		Cliente joao = new Cliente("João", "joao@xyz.com", "3499998888", false);
+
+		ativacaoClienteService.ativar(joao);
+
+		return "Hello!";
 	}
 }
