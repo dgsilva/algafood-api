8e6bad1f22665bc004901ad0ca121b0521c22dc6
Diego Bizerra da Silva <diegorj93@gmail.com>
2023-10-23 15:51:36 -0300

    6.Explorando mais do JPA e Hibernate

--- src/main/java/com/algaworks/agafood/domain/repository/RestauranteRepository.java
+++ src/main/java/com/algaworks/agafood/domain/repository/RestauranteRepository.java
@@ -1,4 +1,4 @@
-package com.algaworks.agafood.domain.repository;
+	package com.algaworks.agafood.domain.repository;
 
 
 import java.math.BigDecimal;
@@ -13,6 +13,12 @@ import com.algaworks.agafood.domain.model.Restaurante;
 
 public interface RestauranteRepository extends CustomJpaRepository<Restaurante, Long>, RestauranteRepositiryQueries,
 JpaSpecificationExecutor<Restaurante> {
+
+	// Errata: se um restaurante não tiver nenhuma forma de pagamento associada a ele,
+	// esse restaurante não será retornado usando JOIN FETCH r.formasPagamento.
+	// Para resolver isso, temos que usar LEFT JOIN FETCH r.formasPagamento
+	@Query("from Restaurante r join fetch r.cozinha left join fetch r.formasPagamento")
+	List<Restaurante> findAll();
 	
 	List<Restaurante> queryByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 	
